7855ce301e9740cd4d4e78c91495cc5ffd55b5ce
pubuzhixing8 <pubuzhixing@gmail.com>
2024-02-25 17:12:38 +0800

    feat: add board to page component (#8)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { Board } from "./components/board";
+import { PlaitElement } from "./interfaces";
+
 export default function Home() {
+  const value = [
+    {
+      id: "Xesdcs",
+      type: "geometry",
+      shape: 'rectangle',
+      points: [
+        [100, 100],
+        [300, 300],
+      ],
+    },
+  ] as PlaitElement[];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Demo  
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Board
+        initialValue={value}
+        options={{}}
+        plaitPlugins={[]}
+        plaitViewport={{ zoom: 1 }}
+      ></Board>
     </main>
   );
 }
--- src/app/plugins/rectangle.tsx
+++ src/app/plugins/rectangle.tsx
@@ -1,9 +1,9 @@
 import { PlaitBoard, Point, RenderElementProps } from "../interfaces";
 
-export const rectPlugin = (board: PlaitBoard) => {
+export const rectanglePlugin = (board: PlaitBoard) => {
     const { drawElement } = board;
     board.drawElement = (props: RenderElementProps) => {
-        if (props.element.type === 'circle') {
+        if (props.element.type === 'geometry' && props.element.shape === 'rectangle') {
             const element = props.element;
             const [start, realEnd] = element.points as Point[];
             const width = Math.abs(realEnd[0] - start[0]);
